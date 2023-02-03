3121a98468b68d8dea70b4d50e15c2558d271e1e
Pratik_vish <Pratikvishwa22@gmail.com>
2023-02-03 20:25:19 +0530

    fix: modify class checking condition

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-let container = document.querySelector("#container")
+
 let dino = document.querySelector("#dino")
 
 //dino jumping code
 window.addEventListener("keydown", (e) => {
     if (e.key == "ArrowUp") {
-        if (dino.classList != "animatedino") {
-            dino.classList.add("animatedino")
+        if(!dino.classList.contains("animatedino")){
+            dino.classList.add("animatedino");
+
             setTimeout(() => {
-                dino.classList.remove("animatedino")
-            }, 500)
+                dino.classList.remove("animatedino");
+            }, 500);
+
         }
     }
 });
