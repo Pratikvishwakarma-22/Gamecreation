641955769134381e9ee89c101063d052b49f00b6
kanha <pratikvishwa22@gmail.com>
2023-02-04 22:41:09 +0530

    style: change code position

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-
-let dino = document.querySelector("#dino")
+let dino = document.querySelector("#dino");
+let block = document.querySelector(".block");
 
 //dino jumping code
 window.addEventListener("keydown", (e) => {
-    if (e.key == "ArrowUp") {
-        if(!dino.classList.contains("animatedino")){
-            dino.classList.add("animatedino");
+  if (e.key == "ArrowUp") {
+    if (!dino.classList.contains("animatedino")) {
+      dino.classList.add("animatedino");
 
-            setTimeout(() => {
-                dino.classList.remove("animatedino");
-            }, 500);
-
-        }
+      setTimeout(() => {
+        dino.classList.remove("animatedino");
+      }, 500);
     }
+  }
 });
-
