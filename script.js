31a87921fbe47b2df591f8319cc21b81e74929eb
Pratik_vish <Pratikvishwa22@gmail.com>
2023-02-07 11:08:07 +0530

    style: format document, add whitespace

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-let dino = document.querySelector(".dino");
-let block = document.querySelector(".block");
-let road = document.querySelector(".road");
-let cloud = document.querySelector(".cloud");
-let score = document.querySelector(".score");
-let variable = document.querySelector(":root");
-let gameover = document.querySelector(".gameover");
+let dino = document.querySelector(".dino")
+let block = document.querySelector(".block")
+let road = document.querySelector(".road")
+let cloud = document.querySelector(".cloud")
+let score = document.querySelector(".score")
+let variable = document.querySelector(":root")
+let gameover = document.querySelector(".gameover")
 
-let playerScore = 0;
+let playerScore = 0
 function scorenum() {
-  playerScore++;
-  score.innerHTML = `Score <b>${playerScore}</b>`;
-  if(playerScore>73){
-    variable_change();
+  playerScore++
+  score.innerHTML = `Score <b>${playerScore}</b>`
+  if (playerScore > 73) {
+    variable_change()
   }
 }
 
-function variable_change(){
-  variable.style.setProperty('--speed','1.6s');
+function variable_change() {
+  variable.style.setProperty('--speed', '1.6s')
 }
 
 //dino jumping code
 window.addEventListener("keydown", (e) => {
   if (e.key == "ArrowUp") {
     if (!dino.classList.contains("animatedino")) {
-      dino.classList.add("animatedino");
+      dino.classList.add("animatedino")
 
       setTimeout(() => {
-        dino.classList.remove("animatedino");
-      }, 500);
+        dino.classList.remove("animatedino")
+      }, 500)
     }
   }
-});
+})
 
 //block animation and starting of game
 window.addEventListener("keydown", (eve) => {
   if (eve.code == "Space") {
     if (!block.classList.contains("animateblock")) {
-      block.classList.add("animateblock");
-      variable.style.setProperty('--speed','2.5s');
-      road.classList.add("animateroad");
-      cloud.classList.add("animatecloud");
-      score_interval = setInterval(scorenum, 200);
-      document.querySelector(".gameover").innerHTML = "";
+      block.classList.add("animateblock")
+      variable.style.setProperty('--speed', '2.5s')
+      road.classList.add("animateroad")
+      cloud.classList.add("animatecloud")
+      score_interval = setInterval(scorenum, 200)
+      document.querySelector(".gameover").innerHTML = ""
     } else if (gameover.innerHTML == "Game Over") {
       console.log("is it working")
       block.classList.remove("animateblock")
@@ -65,18 +65,18 @@ window.addEventListener("keydown", (eve) => {
 
     }
   }
-});
+})
 
 setInterval(() => {
-  let blockleft = parseInt(getComputedStyle(block).getPropertyValue("left"));
-  let dinobottom = parseInt(getComputedStyle(dino).getPropertyValue("bottom"));
-  if (blockleft>-60 && blockleft < 80 && dinobottom < 100) {
-    block.style.animationPlayState = 'paused';
-    road.style.animationPlayState = 'paused';
-    cloud.style.animationPlayState = 'paused';
+  let blockleft = parseInt(getComputedStyle(block).getPropertyValue("left"))
+  let dinobottom = parseInt(getComputedStyle(dino).getPropertyValue("bottom"))
+  if (blockleft > -60 && blockleft < 80 && dinobottom < 100) {
+    block.style.animationPlayState = 'paused'
+    road.style.animationPlayState = 'paused'
+    cloud.style.animationPlayState = 'paused'
 
-    clearInterval(score_interval);
-    playerScore = 0;
-    document.querySelector(".gameover").innerHTML = "Game Over";
+    clearInterval(score_interval)
+    playerScore = 0
+    document.querySelector(".gameover").innerHTML = "Game Over"
   }
-}, 20);
+}, 20)
