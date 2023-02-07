2214a9f473255895d119820dc312f664a777d973
Pratik_vish <Pratikvishwa22@gmail.com>
2023-02-07 11:06:33 +0530

    feat: restart the game on entering space

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let road = document.querySelector(".road");
 let cloud = document.querySelector(".cloud");
 let score = document.querySelector(".score");
 let variable = document.querySelector(":root");
+let gameover = document.querySelector(".gameover");
 
 let playerScore = 0;
 function scorenum() {
@@ -41,6 +42,27 @@ window.addEventListener("keydown", (eve) => {
       cloud.classList.add("animatecloud");
       score_interval = setInterval(scorenum, 200);
       document.querySelector(".gameover").innerHTML = "";
+    } else if (gameover.innerHTML == "Game Over") {
+      console.log("is it working")
+      block.classList.remove("animateblock")
+      road.classList.remove("animateroad")
+      cloud.classList.remove("animatecloud")
+
+      block.style.animationPlayState = 'running'
+      road.style.animationPlayState = 'running'
+      cloud.style.animationPlayState = 'running'
+
+      playerScore = 0
+      score.innerHTML = `Score <b>${playerScore}</b>`
+      document.querySelector(".gameover").innerHTML = ""
+      block.offsetWidth
+
+      block.classList.add("animateblock")
+      variable.style.setProperty('--speed', '2.5s')
+      road.classList.add("animateroad")
+      cloud.classList.add("animatecloud")
+      score_interval = setInterval(scorenum, 200)
+
     }
   }
 });
@@ -48,11 +70,11 @@ window.addEventListener("keydown", (eve) => {
 setInterval(() => {
   let blockleft = parseInt(getComputedStyle(block).getPropertyValue("left"));
   let dinobottom = parseInt(getComputedStyle(dino).getPropertyValue("bottom"));
-  if (blockleft>-60 && blockleft < 80 && dinobottom < 100 ) {
+  if (blockleft>-60 && blockleft < 80 && dinobottom < 100) {
     block.style.animationPlayState = 'paused';
     road.style.animationPlayState = 'paused';
     cloud.style.animationPlayState = 'paused';
-    
+
     clearInterval(score_interval);
     playerScore = 0;
     document.querySelector(".gameover").innerHTML = "Game Over";
